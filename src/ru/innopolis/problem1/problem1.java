0b2592eb54f220d2dc1bf7b69bc04eec38fc8e7f
bulatok <bulat2020205@gmail.com>
2022-04-11 14:55:34 +0300

    init

--- src/ru/innopolis/problem1/problem1.java
+++ src/ru/innopolis/problem1/problem1.java
@@ -1,2 +1,98 @@
 // your solution here
 //fix all the errors including package errors
+package ru.innopolis;
+
+public interface Chain{
+    void setNext(Chain nextChain);
+    void write(Logg logg);
+}
+
+// base class
+public static class Logg{
+    // log message
+    private String message;
+    private String type;
+
+    public Logg(String message, String type){
+        this.message = message;
+        this.type = type;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+    public void setMessage(String newMessage){
+        message = newMessage;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
+public class writeConsole implements Chain{
+    private Chain nextChain;
+
+    public void setNext(Chain nextChain){
+        this.nextChain = nextChain;
+    }
+
+    public void write(Logg logg){
+        if (logg.getType() == "Console"){
+            System.out.println(logg.getMessage());
+        }else{
+            nextChain.write(logg);
+        }
+    }
+}
+
+public class writeFile implements Chain{
+    private Chain nextChain;
+
+    public void setNext(Chain nextChain){
+        this.nextChain = nextChain;
+    }
+
+    public void write(Logg logg){
+        if (logg.getType() == "File"){
+            FileOutputStream outputStream = new FileOutputStream(fileName);
+            outputStream.write(logg.getMessage().getBytes())
+        }else{
+            nextChain.write(logg);
+        }
+    }
+}
+public class writeServer implements Chain{
+    private Chain nextChain;
+
+    public void setNext(Chain nextChain){
+        this.nextChain = nextChain;
+    }
+
+    public void write(Logg logg){
+        if (logg.getType() == "Server"){
+            System.out.println("log simulation from logger - " + logg.getMessage());
+        }else{
+            throw new IllegalArgumentException("unknow source");
+        }
+    }
+}
+
+public class TestProblem1{
+    public static void main(String[] args){
+        Chain ch1 = new writeConsole();
+        Chain ch2 = new writeFile();
+        Chain ch3 = new writeServer();
+
+        ch1.setNext(ch2);
+        ch2.setNext(ch3);
+
+        // this is our request
+        Logg logg = new Logg("log test", "server");
+
+        ch1.write(logg);
+    }
+}
--- src/ru/innopolis/problem2/problem2.java
+++ src/ru/innopolis/problem2/problem2.java
@@ -1,2 +1,69 @@
 // your solution here
 //fix all the errors including package errors
+package ru.innopolis;
+
+public interface Command{
+    void execute(int amount);
+}
+// reciever class
+public class Stock{
+    // for example BTC
+    public void sell(int amount){
+        System.out.printf("you've sold - %d BTC", amount);
+    }
+    public void buy(int amount){
+        System.out.printf("you've bought - %d BTC", amount);
+    }
+}
+public class Switch{
+
+    public void execute(String commandName, int amount){
+        if (commandName == null) throw new IllegalStateException("no command registered for " + commandName);
+
+        if (commandName == "sell"){
+            Command c = new SellCommand();
+            c.execute(amount);
+        }else if (commandName == "buy"){
+            Command c = new BuyCommand();
+            c.execute(amount);
+        }
+    }
+}
+public class BuyCommand implements Command{
+    private final Stock stock;
+
+    public BuyCommand(Stock stock){
+        this.stock = stock;
+    }
+
+    @Override
+    public void execute(int amount){
+        stock.buy(amount);
+    }
+}
+
+public class SellCommand implements Command{
+    private final Stock stock;
+
+    public SellCommand(Stock stock){
+        this.stock = stock;
+    }
+
+    @Override
+    public void execute(int amount){
+        stock.sell(amount);
+    }
+}
+public class TestProblem2{
+    public static void main(String[] args){
+        Stock stock = new Stock();
+
+        Command sell = new SellCommand();
+        Command buy = new BuyCommand();
+
+        Switch sw = new Switch();
+
+        sw.execute("buy", 100);
+        sw.execute("sell", 100);
+    }
+}
